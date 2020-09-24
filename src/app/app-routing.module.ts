6eafb07dc62b2be19d6321727ad9d2fc8f93caa9
Edward <nedinsuyun@gmail.com>
2020-09-24 17:52:35 -0400

    added Errors, Contact Components and Routing for each

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full'                                                           },
+  { path: 'contact',  loadChildren: () => import('./contact/contact-routing.module').then(m => m.ContactRoutingModule)   },
+  { path: 'errors',   loadChildren: () => import('./errors/errors-routing.module').then(m => m.ErrorsRoutingModule)      },
+  { path: '**', redirectTo: '/errors'                                                                     }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ErrorsComponent } from './errors/errors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    ErrorsComponent
   ],
   imports: [
     BrowserModule,
